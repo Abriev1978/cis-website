7571267ce18515a999d3c34e16c0c0e4fb613104
Abriev1978 <167618215+Abriev1978@users.noreply.github.com>
2026-01-19 16:59:40 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "80px", textAlign: "center" }}>
-      <h1>Cyber Infrastructure Solutions</h1>
-      <p>Professional IT Infrastructure & Cloud Services</p>
-    </main>
-  );
-}
